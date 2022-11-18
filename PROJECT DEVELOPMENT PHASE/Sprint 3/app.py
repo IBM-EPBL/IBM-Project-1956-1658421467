bcaef72bd9b42d8f1777f62604ca41dde06229ff
KrishnanRGithub <vinokrish001@gmail.com>
2022-11-18 09:42:40 +0530

    sprint 3 entries

--- PROJECT DEVELOPMENT PHASE/Sprint 3/app.py
+++ PROJECT DEVELOPMENT PHASE/Sprint 3/app.py
@@ -5,12 +5,15 @@ import requests
 import json
 from flask_session import Session
 import utils
-
+from datetime import datetime
 from flask import Flask, session
 from flask_session import Session
 
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
 app.config['SESSION_TYPE'] = 'filesystem'
 PERMANENT_SESSION_LIFETIME = 1800
 app.config['SECRET_KEY'] = 'super secret key'
@@ -23,18 +26,19 @@ Session(app)
 PERMANENT_SESSION_LIFETIME = 1800
 
 
-def check_credentials(u, p):
-    if p == p:
+def check_credentials(e, p):
+    if utils.getPassword(e) == p:
         session['logged_in'] = True
-        session['username'] = u
+        session['email'] = e
         print("Valid User")
-        return render_template('index.html', name=session['username'])
+        return redirect(url_for('dashboard'))
     return render_template('login.html', error="Invalid Credentials")
 
 
 def register(u, p, e):
+    print(u, p, e)
     try:
-        r = utils.addUser(u, p, e)
+        r = utils.addUser(u, e, p)
         if (r == "Username Exists"):
             return render_template('signup.html', error="Username Exists")
         return render_template('login.html')
@@ -42,9 +46,9 @@ def register(u, p, e):
         return render_template('signup.html', error="Error in inserting user")
 
 
-def add_finance_record(u, a, c, d, dt):
+def add_finance_record(e, a, c, d, dt):
     try:
-        r = utils.createFinanceRecord(u, a, c, d, dt)
+        r = utils.createFinanceRecord(e, a, c, d, dt)
         return redirect(url_for('dashboard'))
     except:
         return redirect(url_for('dashboard'))
@@ -53,7 +57,8 @@ def add_finance_record(u, a, c, d, dt):
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return check_credentials(request.form['username'], request.form['password'])
+        print("Checking Credentials")
+        return check_credentials(request.form['email'], request.form['password'])
     else:
         if session.get('logged_in'):
             return redirect(url_for('dashboard'))
@@ -63,19 +68,25 @@ def login():
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     if request.method == 'POST':
-        return add_finance_record(request.form['username'], request.form['category'], request.form['amount'], request.form['description'], request.form['date'])
+        email = session['email']
+        now = datetime.now()
+        dt_string = now.strftime("%Y/%m/%d %H:%M")
+        date = dt_string
+        print(date)
+        return add_finance_record(email, request.form['category'], request.form['amount'], request.form['description'], date)
     else:
         if session.get('logged_in'):
-            username = session['username']
-            rows = utils.fetchFinanceRecord(username)
-            return render_template('dashboard.html', rows=rows)
+            email = session['email']
+            rows = utils.fetchFinanceRecord(email)
+            l = len(rows)
+            return render_template('dashboard.html', rows=rows, len=l)
         return render_template('login.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signin():
     if request.method == 'POST':
-        return register(request.form['username'], request.form['password'], request.form['email'])
+        return register(request.form['name'], request.form['password'], request.form['email'])
     else:
         return render_template('signup.html')
 
@@ -88,4 +99,5 @@ def logout():
 
 if __name__ == "__main__":
     # db.create_all()
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=5000)
--- PROJECT DEVELOPMENT PHASE/Sprint 3/utils.py
+++ PROJECT DEVELOPMENT PHASE/Sprint 3/utils.py
@@ -1,33 +1,37 @@
 from connect import execDB, execReturn
 
 
-def addUser(username, email, password):
-    sql_fd = f"SELECT * FROM user WHERE username='{username}'"
+def addUser(name, email, password):
+    print(name, email, password)
+    sql_fd = f"SELECT * FROM user WHERE email='{email}'"
     r = execReturn(sql_fd)
 
     if r != []:
-        return "Username Exists"
+        return "Email Exists"
 
-    sql_st = f"INSERT INTO user(username , email , pass ) values ( '{username}' , '{email}' , '{password}' )"
+    sql_st = f"INSERT INTO user(name , email , password ) values ( '{name}' , '{email}' , '{password}' )"
     r = execDB(sql_st)
     return "User registered successfully"
 
 
-def getPassword(username):
-    print(username+"is username")
-    sql_fd = f"SELECT pass FROM user WHERE username='{username}'"
+def getPassword(email):
+    sql_fd = f"SELECT password FROM user WHERE email='{email}'"
     r = execReturn(sql_fd)
-    # return r[0]['PASS']
+    # print(r[0])
+    return r[0]['PASSWORD'].strip()
+
+
+def fetchFinanceRecord(email):
+    sql_fd = f"SELECT * FROM finance WHERE email='{email}'"
+    r = execReturn(sql_fd)
     return r
 
 
-def fetchFinanceRecord(username):
-    sql_fd = f"SELECT * FROM financee WHERE username='{username}'"
-    r = execReturn(sql_fd)
-    return r
-
-
-def createFinanceRecord(username, amount, category, description, date):
-    sql_st = f"INSERT INTO financee(username , amount , category , description , date ) values ( '{username}' , '{amount}' , '{category}' , '{description}' , '{date}' )"
+def createFinanceRecord(email, category, amount, description, date):
+    amount = int(amount)
+    amount = 100
+    print("FINANCE", email, amount, category, description, date)
+    sql_st = f"INSERT INTO finance(email , amount , category , description , date ) values ( '{email}' , {amount} , '{category}' , '{description}' , '{date}' )"
     r = execDB(sql_st)
+    print(r)
     return "Record created successfully"
